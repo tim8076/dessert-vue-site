004e9753e0da59acf89a4753d94f0d3a47e95abc
shosetim <shosetim32@gmail.com>
2026-05-11 16:28:05 +0800

    Updates

---
+++ assets/news-3-CrIB1tSR.js
@@ -0,0 +1,1 @@
+const e="/dessert-vue-site/image/home/news-1.svg",s="/dessert-vue-site/image/home/news-2.svg",a="/dessert-vue-site/image/home/news-3.svg";export{s as a,e as b,a as n};
---
+++ assets/statusStore-B_U05I5C.js
@@ -0,0 +1,1 @@
+import{E as t}from"./index-BDW0JOxi.js";const e=t("statusStore",{state:()=>({isLoading:!1})});export{e as s};
